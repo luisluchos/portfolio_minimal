3c971a03db6a6a85aca905e7a7af4ff27d467cf9
Luis Marin <luismgval@gmail.com>
2024-08-27 18:37:14 +0200

    git

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://github.com/luisluchos/portfolio_minimal',  // Reemplaza <tu-usuario> con tu nombre de usuario de GitHub
-  base: '/portfolio_minimal' ,  // Reemplaza "mi-sitio" con el nombre de tu repositorio
+  base: '/portfolio_minimal',  // Reemplaza "mi-sitio" con el nombre de tu repositorio
 });
