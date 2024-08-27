41c3a7ff3174a3a7f4e54e79df666c749b37d7fb
Luis Marin <luismgval@gmail.com>
2024-08-27 18:03:55 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://github.com/luisluchos',
+  base: '/portfolio_minimal',
 });
