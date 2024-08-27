61993bb5385eca2f8776b7d6a0a142a8878608dd
Luis Marin <luismgval@gmail.com>
2024-08-27 19:19:37 +0200

    git

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+	purge: false, // Deshabilitar PurgeCSS temporalmente
+	content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
